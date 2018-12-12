9a401811b77441bb36d10314057ebddb0f85a112
romankrru <rm07ru@gmail.com>
2018-12-12 23:57:06 +0300

    Add code generator

---
+++ src/code-generator/index.js
@@ -0,0 +1,35 @@
+// Code Generator: prints tree into string
+
+function codeGenerator(node) {
+	switch(node.type) {
+		case 'Program':
+			return node.body.map(codeGenerator).join('\n');
+
+		case 'ExpressionStatement':
+			return (
+				codeGenerator(node.expression) + ';'
+			);
+
+		case 'CallExpression':
+			return (
+				codeGenerator(node.callee) +
+				'(' +
+				node.arguments.map(codeGenerator).join(', ') +
+				')'
+			);
+
+		case 'Identifier':
+			return node.name;
+
+		case 'NumberLiteral':
+			return node.value;
+
+		case 'StringLiteral':
+			return '"' + node.value + '"';
+
+		default:
+			throw new TypeError(node.type);
+	}
+}
+
+export default codeGenerator;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import tokenizer from './tokenizer';
 import parser from './parser';
 import transformer from './transformer';
+import codeGenerator from './code-generator';
 
 const tokens = tokenizer(
 `
-(sub 4 5)
 (add 1 4 (div 20 5))
 `
 );
 
 const ast = parser(tokens);
 const newAst = transformer(ast)
-console.log(ast);
-console.log(newAst);
+const compiled = codeGenerator(newAst);
+console.log(compiled);
