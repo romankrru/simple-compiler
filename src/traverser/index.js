ca9f591c8ccbf1c2f51a580be19efc0203898281
romankrru <rm07ru@gmail.com>
2018-12-12 23:23:55 +0300

    Add traverser

---
+++ src/traverser/index.js
@@ -0,0 +1,40 @@
+function traverser(ast, visitor) {
+	traverseArray(array, parent) {
+		array.forEach(child => {
+			traverseNode(child, parent);
+		});
+	}
+
+	function traverseNode(node, parent) {
+		let methods = visitor[node.type];
+
+		if (methods && methods.enter) {
+			methods.enter(node, parent);
+		}
+
+		switch(node.type) {
+			case 'Program':
+				traverseArray(node.body, node);
+				break;
+
+			case 'CallExpression':
+				traverseArray(node.params, node);
+				break;
+
+			case 'NumberLiteral':
+			case 'StringLiteral':
+				break;
+
+			default:
+				throw new TypeError('Unknown node type: ' + node.type);
+		}
+
+		if (methods && methods.exit) {
+			methods.exit(node.parent);
+		}
+	}
+
+	traverseNode(ast, null);
+}
+
+export default traverser;
